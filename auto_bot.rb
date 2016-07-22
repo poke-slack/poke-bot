dc6223dfc6a5b86657512777419615d11c337145
Gal Steinitz <gal@steinitz.com>
2016-07-22 15:12:57 -0700

    random pokes in random channels

--- auto_bot.rb
+++ auto_bot.rb
@@ -3,4 +3,35 @@ require 'logging'
 require 'byebug'
 require_relative 'random_poke.rb'
 
-sleep 30
+logger = Logging.logger(STDOUT)
+logger.level = :debug
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_TOKEN']
+  if not config.token
+    logger.fatal('Missing ENV[SLACK_TOKEN]! Exiting program')
+    exit
+  end
+end
+
+while true do
+  begin
+    client = Slack::RealTime::Client.new
+
+    channel = client.web_client.channels_list.channels.sample
+    channel_name = channel.name
+
+    puts "picked channel: #{channel_name}"
+
+    client.web_client.channels_join({name: channel_name})
+
+
+    RandomPoke.perform(client,channel_name)
+
+    sleep 30
+  rescue StandardError => e
+    puts "EROR!"
+    puts e.message
+    puts e.backtrace
+  end
+end
--- random_poke.rb
+++ random_poke.rb
@@ -1,16 +1,19 @@
 class RandomPoke
-  POKES = ['Blastoise.png','Caterpie.png','Pidgeot.png','Pidgey.png','Rattata.png','avatar.png','charizard.jpg','charmeleon.jpg','metapod.png','venusaur.jpg
-Beedrill.png','Butterfree.png','Kakuna.png','Pidgeotto.png','Raticate.png','Weedle.png','bulbasaur.jpg','charmander.jpg','ivysaur.jpg']
+  POKES = ['Blastoise.png','Caterpie.png','Pidgeot.png','Pidgey.png','Rattata.png','avatar.png','charizard.jpg','charmeleon.jpg','metapod.png','venusaur.jpg','Beedrill.png','Butterfree.png','Kakuna.png','Pidgeotto.png','Raticate.png','Weedle.png','bulbasaur.jpg','charmander.jpg','ivysaur.jpg']
 
-  def self.perform(client)
-    self.new.perform(client)
+  def self.perform(client,channel_name)
+    self.new.perform(client,channel_name)
   end
 
-  def perform(client)
+  def perform(client,channel_name)
     poke = POKES.sample
 
+    puts "picked pokemon: #{poke}"
+
+    puts "uploading!"
+
     client.web_client.files_upload(
-        channels: '#general',
+        channels: "#{channel_name}",
         as_user: true,
         file: Faraday::UploadIO.new("resources/#{poke}", 'image/jpg'),
         title: poke,
